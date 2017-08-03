95e3469a50fd3d4761b30912d35ed6459657ee56
Danila <fourlessthanthree@yahoo.com>
2017-08-04 00:02:21 +0300

    Fix grammar

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -27,10 +27,10 @@ const renderUsers = (presences) => {
 
 
 const getDataFromDiff = (diff, key) => {
-  const messageType = key === 'join' ? 'joined' : 'leaved';
+  const messageType = key === 'join' ? 'joined' : 'left';
   const message = document.createElement('div');
   message.className = key;
-  message.innerHTML = Object.keys(diff[`${key}s`]).reduce((acc, name) => `${acc}\n<i>${name} ${messageType} the channel</i><br>`, '');
+  message.innerHTML = Object.keys(diff[`${key}s`]).reduce((acc, name) => `${acc}\n<i>${name} ${messageType} channel</i><br>`, '');
 
   return message;
 };
